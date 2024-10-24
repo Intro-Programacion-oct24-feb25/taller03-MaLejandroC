122feda9ec5fbf42605275033a9e48a7487e5350
MalejandroC <macarrion44@utpl.edu.ec>
2024-10-24 11:35:29 -0500

    cambios

---
+++ Ejercicio04/src/ejercicio04/Ejercicio04.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio04;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here}
+        int var = 20;
+        int var1 = 1;
+        int var2 = 3;
+        int var3 = 2;
+        int var4 = 3;
+        
+        boolean resultado = var/(var1+var2) <= var4;
+        
+        System.out.println(resultado);
+  
+    }
+    
+}
