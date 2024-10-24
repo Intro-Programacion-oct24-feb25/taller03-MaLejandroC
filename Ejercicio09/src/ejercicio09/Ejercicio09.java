5cf93df144bde577bd44f279d10130b9d84bd102
unknown <macarrion44@utpl.edu.ec>
2024-10-24 12:29:21 -0500

    ejercicio09

---
+++ Ejercicio09/src/ejercicio09/Ejercicio09.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio09;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int var = 81;
+        int var1 = 25;
+        int var2 = 10;
+        int var3 = 50;
+        
+        boolean resultado = Math.sqrt (var)+ Math.sqrt(var1)*var2 >= var3;
+        
+        System.out.println(resultado);
+
+    }
+    
+}
