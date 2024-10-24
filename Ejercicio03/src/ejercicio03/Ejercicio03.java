5303f676c8bb8fdbbace1962215961c57af54546
MalejandroC <macarrion44@utpl.edu.ec>
2024-10-24 11:26:11 -0500

    cambios

--- Ejercicio03/src/ejercicio03/Ejercicio03.java
+++ Ejercicio03/src/ejercicio03/Ejercicio03.java
@@ -16,7 +16,19 @@ public class Ejercicio03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int var = 10;
+        int var1 = 2;
+        int var2 = 16;
+        int var3 = 20;
+        int var4 = 1;
+        int var5 = 11;
+        double resultado = Math.pow(var, var1)+Math.sqrt(var2)+var3/(var4+var5);
         
+        System.out.println(resultado);
+     
+    
+                
+    
     }
     
 }
